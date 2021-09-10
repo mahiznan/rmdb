650658022bcb3dcbfa3395cdbf33b1eb46413503
Rajeshkumar <rkumar@ctc-g.com.sg>
2021-09-10 08:40:15 +0800

    add route features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./GlobalStyle";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 const App = () => (
-  <div>
-    Start here.
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
     <GlobalStyle />
-  </div>
+  </Router>
 );
 
 export default App;
